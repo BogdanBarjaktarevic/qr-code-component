61abc4acc539d7d94e40852323142009913a7115
Bogdan Barjaktarevic <barjaktarevicbogdan@gmail.com>
2022-11-21 14:45:41 +0100

    create QR code component

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#ffffff",
+      lightGray: "#d6e2f0",
+      grayishBlue: "#7b879d",
+      darkBlue: "#1f3251",
+    },
+  },
+  plugins: [],
+};
